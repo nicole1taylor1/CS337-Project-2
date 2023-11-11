38609fda3ebda6d3351dd4b4e829d32f1b2c3feb
Nicole Taylor <nicole1taylor1@gmail.com>
2023-11-11 12:59:03 -0600

    ingredients list

--- parser_1.py
+++ parser_1.py
@@ -9,3 +9,9 @@ def read_recipe_from_url(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     return soup
+
+def get_ingredients_from_soup(soup):
+    ingredients = soup.find("div", {"id":"mntl-structured-ingredients_1-0"})
+    #get entire list of ingredients 
+    for child in ingredients.find_all("li"):
+        text = child.text.replace("\n", "").strip()
